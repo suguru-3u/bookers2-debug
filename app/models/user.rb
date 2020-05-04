bd69371e3173b089cdf95be20fe470056608a71d
vagrant <vagrant@localhost.localdomain>
2020-05-04 07:26:29 +0000

    [Finsih] 応用課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
-  
+
   has_many :favorites, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
